256a5660e53667c9728a7a6b9a904557cdb3fa50
omxrprob <omarkorji2@gmail.com>
2025-07-02 19:04:02 +0200

    popup.js

--- popup.js
+++ popup.js
@@ -5,29 +5,30 @@ document.addEventListener('DOMContentLoaded', function () {
       "https://tv.garden",
       "https://lostgamer.io",
       "https://pointerpointer.com",
-      "https://drivenlisten.com",
-      "https://medoob.com",          // Assumed URL for "me doob"
+      "https://driveandlisten.herokuapp.com",
+      "https://mrdoob.com/",
       "https://slowroads.io",
       "https://virtualvacation.us",
-      "https://emulatorgames.online",
-      "https://ocearch.org",
-      "https://planyourroom.com",
-      "https://huggingface.com",
-      "https://timemap.org",
-      "https://spottyedrowingchild.com",
-      "https://whatsmystarbucksname.com",
-      "https://lookingatsomething.com",
-      "https://screamintothevoid.com",
-      "https://geo-fs.com",
-      "https://yarn.co",
+      "https://www.emulatorgames.online/",
+      "https://www.ocearch.org/",
+      "https://www.planyourroom.com/",
+      "https://huggingface.co/",
+      "https://timemap.org/",
+      "https://spottyedrowingchild.com/",
+      "https://whatsmystarbucksname.com/",
+      "https://lookingatsomething.com/",
+      "https://screamintothevoid.com/",
+      "https://geo-fs.com/",
+      "https://yarn.co/",
       "https://sites.google.com/view/nextbitgames/home",
-      "https://playglenn.com",
-      "https://kickassapp.com",
-      "https://ismycomputeron.com",
+      "https://playglenn.com/",
+      "https://kickassapp.com/",
+      "https://ismycomputeron.com/",
       "https://dood.al/pinktrombone/",
-      "https://retrogames.on",
-      "https://zty.pe"
+      "https://www.retrogames.cc/",
+      "https://zty.pe/"
     ];
+    
     const randomIndex = Math.floor(Math.random() * websites.length);
     const randomWebsite = websites[randomIndex];
     chrome.tabs.create({ url: randomWebsite });
